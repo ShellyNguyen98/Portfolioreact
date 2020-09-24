5183135c7f9464752b50cb5c69d3c216c553122b
Trang Nguyen <ShellyNguyen98@gmail.com>
2020-09-23 23:12:42 -0700

    Updates

---
+++ precache-manifest.20f006211dcf0bb65a4de962c7496c64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "676d1dc12606f583b52bc61303128eb5",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "fac85897a4fd31bb54a6",
+    "url": "/react-gh-pages/static/css/main.93807bfa.chunk.css"
+  },
+  {
+    "revision": "fd55b4d93aed8eabe295",
+    "url": "/react-gh-pages/static/js/2.91bcf0d5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-gh-pages/static/js/2.91bcf0d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fac85897a4fd31bb54a6",
+    "url": "/react-gh-pages/static/js/main.009623b2.chunk.js"
+  },
+  {
+    "revision": "db877ac2a7df6a34ec79",
+    "url": "/react-gh-pages/static/js/runtime-main.647838b0.js"
+  }
+]);
