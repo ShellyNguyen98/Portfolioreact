ba8a2a50ee1b29d080661f74df45ecaad546e52e
Trang Nguyen <ShellyNguyen98@gmail.com>
2020-09-23 20:15:41 -0700

    update

---
+++ src/components/HeroSection.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.css';
+
+import './HeroSection.css';
+
+const HeroSection = () => {
+    return (
+        <div className= 'hero-container'>
+            <h1>
+                Welcome to my page
+            </h1>
+        </div>
+    )
+}
+
+export default HeroSection
